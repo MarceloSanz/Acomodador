915b1f87f28e56dd69518a69b9ea1bb62dfc2aa0
marcelo.herce <marcelo.herce@babelgroup.com>
2024-04-25 12:28:46 +0200

    fix

--- src/main/java/org/example/service/GestionarSala.java
+++ src/main/java/org/example/service/GestionarSala.java
@@ -2,7 +2,9 @@ package org.example.service;
 
 import org.example.model.ISalaRepository;
 import org.example.model.Sala;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GestionarSala implements IGestionarSala{
     ISalaRepository salaRepository;
     public GestionarSala(ISalaRepository salaRepository){
